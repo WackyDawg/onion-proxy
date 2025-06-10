42d3997c4ac27cec85b926053b7c40577d20aca3
WackyDawg <juliannwadinobi098@gmail.com>
2025-06-10 03:54:45 +0100

    test: update test endpoint for Tor IP verification
    The previous IP verification endpoint (ipify.org) was replaced with bigdatacloud.net for more reliable IP detection in test environment.

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const onionProxy = require('./app.js');
 onionProxy.startTorProxy(() => {
-  onionProxy.requestThroughTor('https://api.ipify.org', (err, ip) => {
+  onionProxy.requestThroughTor('https://api.bigdatacloud.net/data/client-ip', (err, ip) => {
     if (err) return console.error(err);
     console.log('Your Tor IP:', ip);
   });
